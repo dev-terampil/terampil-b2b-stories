b210ede4433c6aa18b2c0b7354af5323baaec95a
Rio <rio@graf-research.com>
2024-12-04 22:51:07 +0700

    init

---
+++ dist/index.d.ts
@@ -0,0 +1,134 @@
+import React, { ButtonHTMLAttributes, HTMLAttributes, InputHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    leftIconURL?: string;
+    rightIconURL?: string;
+    outline?: boolean;
+}
+declare function Button(props: ButtonProps): React.JSX.Element;
+
+interface ProgressValueProps {
+    value: number;
+}
+declare function ProgressValue(props: ProgressValueProps): React.JSX.Element;
+
+interface ProgressValueThreeProps {
+    valueComplete: number;
+    valueProgress: number;
+    valuePending: number;
+}
+declare function ProgressValueThree(props: ProgressValueThreeProps): React.JSX.Element;
+
+type BadgeCollectionType = 'check' | 'timeout' | 'half-star' | 'red' | 'yellow' | 'green';
+interface BadgeCollectionProps {
+    type: BadgeCollectionType;
+    children?: any;
+    bigger?: boolean;
+}
+declare function BadgeCollection(props: BadgeCollectionProps): React.JSX.Element;
+
+interface TabV1Props {
+}
+declare function TabV1(props: TabV1Props): React.JSX.Element;
+
+interface TabV2Props {
+}
+declare function TabV2(props: TabV2Props): React.JSX.Element;
+
+interface OptionData {
+    label: string;
+    value: any;
+}
+interface InputDropdownProps {
+    options: OptionData[];
+    onChange?(value: any): void;
+    onChanges?(value: any[]): void;
+    value?: any;
+    values?: any[];
+    className?: string;
+    placeholder?: string;
+    multivalue?: boolean;
+}
+declare function InputDropdown(props: InputDropdownProps): React.JSX.Element;
+
+interface TableMeta<T> {
+    columns: TableMetaColumn[];
+    renderRow(row: T, i: number): JSX.Element;
+}
+interface TableMetaColumn {
+    label: string;
+    filter?: TableMetaColumnFilter;
+}
+type TableMetaColumnFilter = TableMetaColumnFilterText | TableMetaColumnFilterOptionSingleValue | TableMetaColumnFilterOptionMultiValue;
+interface TableMetaColumnFilterText {
+    type: 'text';
+    placeholder?: string;
+    onSubmit(value: any): void;
+}
+interface TableMetaColumnFilterOptionSingleValue {
+    type: 'option-single-value';
+    placeholder?: string;
+    options: OptionData[];
+    onSubmit(value: any): void;
+}
+interface TableMetaColumnFilterOptionMultiValue {
+    type: 'option-multi-value';
+    placeholder?: string;
+    options: OptionData[];
+    onSubmit(values: any[]): void;
+}
+
+interface TableProps<T> {
+    meta: TableMeta<T>;
+    data: T[];
+    numActions?: number;
+}
+declare function Table<T>(props: TableProps<T>): React.JSX.Element;
+
+interface NewHeaderProps {
+    iconUrl?: string;
+    title?: string;
+    removeTokenCookie(): void;
+}
+declare function NewHeader(props: NewHeaderProps): React.JSX.Element;
+
+interface NewRightNavProps {
+}
+declare function NewRightNav(props: NewRightNavProps): React.JSX.Element;
+
+declare enum RoleSBType {
+    PERSONAL = "PERSONAL",
+    TEAM = "TEAM",
+    CEO = "CEO",
+    HR = "HR"
+}
+
+interface NewSidebarProps {
+    mode: RoleSBType;
+    setSidebarMode(mode: RoleSBType): void;
+}
+declare function NewSidebar(props: NewSidebarProps): React.JSX.Element;
+
+interface NewTemplateProps extends HTMLAttributes<HTMLDivElement> {
+    title?: string;
+    titleIconUrl?: string;
+    mode: RoleSBType;
+    setSidebarMode(mode: RoleSBType): void;
+    removeTokenCookie(): void;
+}
+declare function NewTemplate(props: NewTemplateProps): React.JSX.Element;
+
+declare function useOutsideClick(ref: any, callback: (e: any) => void): void;
+
+interface InputTextProps extends InputHTMLAttributes<HTMLInputElement> {
+}
+declare function InputText(props: InputTextProps): React.JSX.Element;
+
+interface LabelProps {
+    label: string;
+    required?: boolean;
+    children?: any;
+}
+declare function Label(props: LabelProps): React.JSX.Element;
+
+export { BadgeCollection, Button, InputDropdown, InputText, Label, NewHeader, NewRightNav, NewSidebar, NewTemplate, type OptionData, ProgressValue, ProgressValueThree, TabV1, TabV2, Table, type TableMeta, type TableMetaColumn, type TableMetaColumnFilter, type TableMetaColumnFilterOptionMultiValue, type TableMetaColumnFilterOptionSingleValue, type TableMetaColumnFilterText, useOutsideClick };
